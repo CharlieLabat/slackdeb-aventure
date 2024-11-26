f84239023c5aa3107a20bf5527031fc68318ffb6
Charlie LABAT <charlielabat17@gmail.com>
2024-11-26 12:09:39 +0100

    Modif style et élément de Texte

--- src/components/Texte.jsx
+++ src/components/Texte.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 const Texte = () => {
-  return <section className="texte">
-<p className="texte__dialogue">
-  
-</p>
-<input type="text" name="" id=""  className="texte__commande" />
-  </section>;
+  return (
+    <section className="texte">
+      <p className="texte__dialogue">Texte</p>
+      <label htmlFor="commande" className="texte__hote"></label>
+      <input type="text" name="commande" id="commande" className="texte__commande" />
+    </section>
+  );
 };
 
 export default Texte;
