02d7a87d2ae994551caae95d6efdee9e6e39c859
Charlie LABAT <charlielabat17@gmail.com>
2025-01-13 15:43:46 +0100

    Ajout du code de sauvegarde du jeu

--- src/hooks/saveGame.js
+++ src/hooks/saveGame.js
@@ -1,8 +1,11 @@
 
 import Donnee from "../data/data.json";
-
-function saveGame(id){
-
+// Fonction de sauvegarde 
+function saveGame(id, nivId){
+ const save = Donnee.saves[id]
+ const niveauNom = Donnee.levels[nivId]["nom-niveau"]
+ save.niveau = niveauNom
+ save["niveau-id"] = nivId
 }
 
 export default saveGame;
