46c87618cc8e85149982d199951e843dea4ace72
Charlie LABAT <charlielabat17@gmail.com>
2025-01-24 12:32:10 +0100

    Intetgation action cmd + commentaire

--- src/components/Texte.jsx
+++ src/components/Texte.jsx
@@ -1,11 +1,37 @@
-import React from "react";
-import useCommand from "../hooks/useCommand";
+import React, { useState } from "react";
+import listeCommand from "../hooks/listeCommand";
+
 const Texte = () => {
+  const [commande, setCommande] = useState(""); // Stock la commande du terminal
+  const [idLevel, setIdLevel] = useState(0) // Stock l'id du niveau courant 
+  const [imageLevel, setImageLevel] = useState(["", ""]) // Stock l'image et son nom (alt)
+  // Gere la récupération de la commande 
+  const handlePrompt = (evt) => {
+    setCommande(evt.target.value);
+    
+  };
+  // Gere l'envoi vers les actions du jeu 
+  const handlePromptSoumis = (evt) => {
+    evt.preventDefault();
+    console.log(commande);
+    const cmdListe = commande.split(" ")
+    console.log(cmdListe)
+    if (cmdListe.length === 3) { // Si un id existe 
+      listeCommand(cmdListe)
+    }
+    else{
+      cmdListe.push("")
+      cmdListe.push("0")
+
+      listeCommand(cmdListe)
+    }
+
+  };
   return (
     <section className="texte">
       <p className="texte__dialogue">Texte</p>
       <p className="texte__instruction">Ici liste des commandes</p>
-      <div className="texte__interaction">
+      <form className="texte__interaction" onSubmit={handlePromptSoumis}>
         <label htmlFor="commande" className="texte__hote">
           lejoueur@sda <span className="texte__hote--chemin">~</span>
           <span className="texte__hote--utilisateur">$</span>
@@ -16,9 +42,10 @@ const Texte = () => {
           id="commande"
           className="texte__commande"
           placeholder="_"
-          onInput={}
+          value={commande}
+          onChange={handlePrompt}
         />
-      </div>
+      </form>
     </section>
   );
 };
---
+++ src/hooks/listeCommand.js
@@ -0,0 +1,30 @@
+import Donnee from "../data/data.json";
+import saveGame from "./saveGame";
+
+
+function listeCommand(cmdEle) {
+    // Liste des commandes valide selon le niveau courant
+    const listeCmd = Donnee.levels[cmdEle[2]]["liste-cmd"]
+    const cmdExiste = listeCmd.includes(cmdEle[0])
+    console.log(listeCmd)
+    console.log(cmdEle)
+    // Si la commande est dans la liste des commandes du niveau et qui est gere ...
+    if (cmdExiste) {
+        switch (cmdEle[0]) {
+            case "start": // Commence le jeu (Uniquement l'écran d'accueil)
+                console.log("Lestgo !!!")
+                break
+            case "save": // Sauvegarde le jeu
+                saveGame(cmdEle[1], cmdEle[2])
+                break
+
+            case "load": // Charge le jeu
+                break
+            default:
+                console.log("La commande n'existe pas !")
+                break;
+        }
+    }
+
+}
+export default listeCommand;
